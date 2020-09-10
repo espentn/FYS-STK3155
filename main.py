190983106d8c4d57134e0150d8343a5ee0172756
espentn <espentn@gmail.com>
2020-09-10 23:18:04 +0200

    Added data set splitting, fitting of data etc

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 from random import random, seed
 from scikot-learn import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
@@ -18,20 +19,24 @@ def frankeFunction(x,y):
 	term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
 	return term1 + term2 + term3 + term4 #+ noise
 
-def MSE(z_data, z_model):
-	n = np.size(z_model)
-	return np.sum((z_data-z_model)**2)/n
+def createDataPoints(x,y):
+	x_d, y_d = np.meshgrid(x,y)
+	z_d = FrankeFunction(x_d,y_d)
+	return x_d, y_d, z_d
 
-def R2(z_data, z_model):
-	n = np.size(z_data)
-	return 1 - np.sum((z_data-z_model)**2)/np.sum((z_data-(np.sum(z_data)/n))**2)
+# Need to flatten the matrices to be able to compute the beta
+def convertDataPoints(x,y,z):
+	x_d = np.ravel(x)
+	y_d = np.ravel(y)
+	z_d = np.ravel(z)
+	return x_d, y_d, z_d
 
-def create_design_matrix(x, y, n):
+def create_design_matrix(x, y):
 	if len(x.shape) > 1:
 		x = np.ravel(x)
 		y = np.ravel(y)
 
-	N = len(x)
+	n = len(x)
 	p = int((n+1)*(n+2)/2)
 	X = np.ones((N,p))
 
@@ -42,13 +47,37 @@ def create_design_matrix(x, y, n):
 	return X
 
 
+def fitBeta(self,X, z_data):
+	beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(z_data)
+    return beta.T
+
+def trainData(x,z):
+    X_train, X_test, z_train, z_test = train_test_split(x,z,test_size=0.3)
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train_scaled = scaler.transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+    Beta = np.linalg.inv(X_train.T.dot(X_train)).dot(X_train.T).dot(z_train)
+    ztilde = X_train @ Beta
+    return ztilde
+
+def MSE(z_data, z_model):
+	n = np.size(z_model)
+	return np.sum((z_data-z_model)**2)/n
+
+def R2(z_data, z_model):
+	n = np.size(z_data)
+	return 1 - np.sum((z_data-z_model)**2)/np.sum((z_data-(np.sum(z_data)/n))**2)
+
+
+
 # Make data.
 x = np.arange(0, 1, 0.05)
 y = np.arange(0, 1, 0.05)
 noise = np.random.rand()
 
 x, y = np.meshgrid(x,y)
-z = FrankeFunction(x, y)
+z = frankeFunction(x, y)
 
 # Plot the surface.
 surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
